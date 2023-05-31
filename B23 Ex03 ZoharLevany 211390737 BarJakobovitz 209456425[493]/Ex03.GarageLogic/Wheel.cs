db39f8975a3fe6f0eba436291fa6b5b3888e53bb
Bar Jakobovitz <barzk98@gmail.com>
2023-05-31 12:19:25 +0300

    wheel class

---
+++ B23 Ex03 ZoharLevany 211390737 BarJakobovitz 209456425[493]/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Wheel
+    {
+        String ManufacturerName;
+        float CurrentAirPressure;
+        float MaxAirPressure;
+
+        internal Wheel(string i_manufacturerName, float i_currentAirPressure, float i_maxAirPressure)
+        {
+            ManufacturerName = i_manufacturerName;
+            CurrentAirPressure = i_currentAirPressure;
+            MaxAirPressure = i_maxAirPressure;
+        }
+        internal void InflateAction(float i_airToAdd)
+        {
+          if (i_airToAdd+CurrentAirPressure<MaxAirPressure)
+            {
+                CurrentAirPressure += i_airToAdd;
+            }
+        }
+
+    }
+}
