af667193b40e5d8ddf5d66829430c99c9dc049f9
Bar Jakobovitz <barzk98@gmail.com>
2023-06-01 14:41:33 +0300

    Add VehicleInGarage

---
+++ B23 Ex03 ZoharLevany 211390737 BarJakobovitz 209456425[493]/Ex03.GarageLogic/VehicleInGarage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class VehicleInGarage
+    {
+        private readonly Vehicle r_vehicle;
+        private readonly String r_OwnerName;
+        private readonly String r_OwnerPhoneNumber;
+        private eVehicleStatus m_VehicleStatus;
+
+        public VehicleInGarage(Vehicle i_Vehicle, string i_OwnerName, string i_OwnerPhoneNumber, eVehicleStatus i_VehicleStatus)
+        {
+            r_vehicle = i_Vehicle;
+            r_OwnerName = i_OwnerName;
+            r_OwnerPhoneNumber = i_OwnerPhoneNumber;
+            m_VehicleStatus = i_VehicleStatus;
+        }
+    }
+}
