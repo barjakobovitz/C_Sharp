a4e2494a8108463546a053007323d64436f25cbd
Bar Jakobovitz <barzk98@gmail.com>
2023-06-01 13:05:15 +0300

    Engine class

---
+++ B23 Ex03 ZoharLevany 211390737 BarJakobovitz 209456425[493]/Ex03.GarageLogic/FuelEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class FuelEngine : xEngine
+    {
+
+        internal FuelEngine(float i_RemainingEnergy, float i_MaxEnergy) : base(i_RemainingEnergy, i_MaxEnergy)
+        {
+        }
+        internal void Refueling(float i_FuelToAdd, eFuelType i_FuelType)
+        {
+            if ((RemainingEnergy + i_FuelToAdd) < MaxEnergy)
+            {
+                RemainingEnergy += i_FuelToAdd;
+            }
+        }
+
+    }
+}
---
+++ B23 Ex03 ZoharLevany 211390737 BarJakobovitz 209456425[493]/Ex03.GarageLogic/xEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class xEngine
+    {
+        private float m_RemainingEnergy;
+        private readonly float r_MaxEnergy;
+
+        internal xEngine(float i_RemainingEnergy, float i_MaxEnergy)
+        {
+            r_MaxEnergy = i_MaxEnergy;
+            m_RemainingEnergy = i_RemainingEnergy;
+        }
+
+        public float MaxEnergy
+        {
+            get
+            {
+                return r_MaxEnergy;
+            }
+
+        }
+
+        public float RemainingEnergy
+        {
+            get
+            {
+                return m_RemainingEnergy;
+            }
+            set
+            {
+                m_RemainingEnergy = value;
+            }
+
+        }
+    }
+}
